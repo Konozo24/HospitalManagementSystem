2f8dfa023203183ea3867e7c6f86d89bf40fca38
Konozo24 <mingwei12242016@gmail.com>
2025-04-22 17:44:30 +0800

    Update MedicalProfessional.java

--- Staff/MedicalProfessional.java
+++ Staff/MedicalProfessional.java
@@ -1,29 +1,75 @@
-public class MedicalProfessional extends Person {
+package Assignment;
+
+// Abstract class for medical professionals (doctors and nurses)
+abstract class MedicalProfessional extends Person {
     private String employeeId;
     private double salary;
     private String joinDate;
     private String department;
     private String qualification;
-
-    // Constructor
-    public MedicalProfessional(String id, String name, int age, String gender, String employeeId, double salary, String joinDate, String department, String qualification) {
-        super(id, name, age, gender);
+    
+    public MedicalProfessional(String id, String name, String address, String phoneNumber, 
+                              String email, String emergencyContact, String dateOfBirth, 
+                              char gender, String employeeId, double salary, String joinDate, 
+                              String department, String qualification) {
+        super(id, name, address, phoneNumber, email, emergencyContact, dateOfBirth, gender);
         this.employeeId = employeeId;
         this.salary = salary;
         this.joinDate = joinDate;
         this.department = department;
         this.qualification = qualification;
     }
-    public String toString() {
-        return "Medical Professional: " + name + 
-               "\nEmployee ID: " + employeeId + 
-               "\nSalary: $" + salary + 
-               "\nJoin Date: " + joinDate + 
-               "\nDepartment: " + department + 
-               "\nQualification: " + qualification;
+
+    // Getters and Setters
+    public String getEmployeeId() {
+        return employeeId;
     }
 
-    public String getDepartment(){
-        return this.department;
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(String joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+    
+    // Abstract method to be implemented by Doctor and Nurse
+    public abstract String getProfessionalInfo();
+    
+    @Override
+    public String toString() {
+        return super.toString() + 
+               "\nEmployee ID: " + employeeId + 
+               "\nDepartment: " + department + 
+               "\nQualification: " + qualification + 
+               "\nJoin Date: " + joinDate;
     }
 }
