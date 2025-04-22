838cd59982fa29e5cb0225885ea4165dc0312c8c
Konozo24 <mingwei12242016@gmail.com>
2025-04-22 17:44:01 +0800

    Update and rename Nurse to Nurse.java

---
+++ Staff/Nurse.java
@@ -0,0 +1,68 @@
+package Assignment;
+
+class Nurse extends MedicalProfessional {
+    private String assignedDoctor;
+    private String shift;
+    private String duties;
+    
+    public Nurse(String id, String name, String address, String phoneNumber, 
+                String email, String emergencyContact, String dateOfBirth, 
+                char gender, String employeeId, double salary, String joinDate, 
+                String department, String qualification, String assignedDoctor,
+                String shift, String duties) {
+        super(id, name, address, phoneNumber, email, emergencyContact, dateOfBirth, 
+              gender, employeeId, salary, joinDate, department, qualification);
+        this.assignedDoctor = assignedDoctor;
+        this.shift = shift;
+        this.duties = duties;
+    }
+
+    // Getters and Setters
+    public String getAssignedDoctor() {
+        return assignedDoctor;
+    }
+
+    public void setAssignedDoctor(String assignedDoctor) {
+        this.assignedDoctor = assignedDoctor;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getDuties() {
+        return duties;
+    }
+
+    public void setDuties(String duties) {
+        this.duties = duties;
+    }
+    
+    // Implementation of abstract methods
+    @Override
+    public String getRole() {
+        return "Nurse";
+    }
+    
+    @Override
+    public String getDetails() {
+        return "Nurse: " + getName() + ", Department: " + getDepartment();
+    }
+    
+    @Override
+    public String getProfessionalInfo() {
+        return "Department: " + getDepartment() + ", Shift: " + shift + ", Assigned Doctor: " + assignedDoctor;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + 
+               "\nAssigned Doctor: " + assignedDoctor + 
+               "\nShift: " + shift + 
+               "\nDuties: " + duties;
+    }
+}
