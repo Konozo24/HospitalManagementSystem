a4d776276fb9a8fc8f17976a0bdc014d2b2b2932
Kelvin061028 <kelvinliew2752@gmail.com>
2025-04-27 21:03:59 +0800

    Create DateValidator.java

---
+++ HMS_UI/Patient Module /DateValidator.java
@@ -0,0 +1,18 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateValidator {
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static boolean isValidDate(String date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        dateFormat.setLenient(false); // This makes sure the date is strictly parsed
+        try {
+            Date parsedDate = dateFormat.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+}
